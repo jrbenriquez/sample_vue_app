c11aed52068d07a47ed9177919d89465e84de840
John Rei Enriquez <johnrei.enriquez@gmail.com>
2018-05-17 12:20:58 +0000

    Initial Commit

---
+++ src/components/Page.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="page">
+        <h1>{{ welcomeMessage }}</h1>
+        I am {{fullName}}
+        <div>
+            <br><h4>Tell me your name</h4>
+            <input type="text" v-model="yourName">
+        </div>
+        <div>
+            <h2>Hello{{ catchedName }}!</h2>
+        </div>
+        <todo></todo>
+        <div class="centerized">Created with:</div>
+        <div>
+            <img id="vue-logo" src="../assets/logo.png">
+        </div>
+        <div>
+            <p>2018</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import Todo from './Todo'
+
+export default {
+  name: 'page',
+  data () {
+    return {
+      welcomeMessage: 'Welcome to my HOME!',
+      fullName: 'John Rei B. Enriquez',
+      yourName: ''
+    }
+  },
+  components: {
+    Todo
+  },
+  methods: {
+
+  },
+  computed: {
+    // a computed getter
+    catchedName: function () {
+      // `this` points to the vm instance
+      if (this.yourName === '') {
+        return ''
+      }
+      return ' ' + this.yourName
+    }
+  },
+  created: function () {
+    this.$http.get('https://jsonplaceholder.typicode.com/users')
+      .then(function (response) {
+        console.log(response.data)
+      })
+  }
+}
+
+</script>
+
+<style>
+    #vue-logo {
+        height: 2em;
+        padding: 0em 0 0 0;
+    }
+    .centerized {
+        vertical-align: middle;
+        line-height: 50px;
+    }
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,75 @@
+/* eslint no-undef: 0 */
+/* eslint-disable no-alert, no-console */
+
+<template>
+  <div id="maintodo">
+    <div>Write down what you want done</div>
+    <input type='text' v-on:keyup.enter="addTask" v-model='newTask'>
+    <ul id="tasklist">
+        <li v-for="task in tasks" :key="task">
+            {{ task }}
+        </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data () {
+    return {
+      newTask: '',
+      tasks: []
+    }
+  },
+  methods: {
+    addTask: function (e) {
+      this.tasks.push(this.newTask)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+h2 {
+  font: 400 40px/1.5 Helvetica, Verdana, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  font: 200 20px/1.5 Helvetica, Verdana, sans-serif;
+  border-bottom: 1px solid #ccc;
+}
+
+li:last-child {
+  border: none;
+}
+
+li a {
+  text-decoration: none;
+  color: #000;
+  display: block;
+  width: 200px;
+
+  -webkit-transition: font-size 0.3s ease, background-color 0.3s ease;
+  -moz-transition: font-size 0.3s ease, background-color 0.3s ease;
+  -o-transition: font-size 0.3s ease, background-color 0.3s ease;
+  -ms-transition: font-size 0.3s ease, background-color 0.3s ease;
+  transition: font-size 0.3s ease, background-color 0.3s ease;
+}
+
+li a:hover {
+  font-size: 30px;
+  background: #f6f6f6;
+}
+</style>
+/* eslint-enable no-alert */
